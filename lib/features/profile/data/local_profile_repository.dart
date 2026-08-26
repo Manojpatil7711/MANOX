2d811ef4f1f10d5aeb76df06777bb0d7c5238ff3
Manojpatil7711 <90497823+Manojpatil7711@users.noreply.github.com>
2026-08-27 00:33:09 +0530

    fix: keep local profile repository compatible with gender field

--- lib/features/profile/data/local_profile_repository.dart
+++ lib/features/profile/data/local_profile_repository.dart
@@ -6,6 +6,7 @@ class LocalProfileRepository implements ProfileRepository {
   @override Future<ProfileData> fetchProfile() async => demoProfile;
   @override Future<ProfileData> fetchProfileByUserId(String userId) async => demoProfile;
   @override Future<List<String>> fetchPostIds() async => demoProfile.postIds;
-  @override Future<ProfileData> updateProfile({required String displayName,required String username,required String bio,String? avatarPath}) async => ProfileData(id:demoProfile.id,displayName:displayName,handle:'@${username.replaceFirst(RegExp(r'^@+'),'')}',bio:bio,isCreator:demoProfile.isCreator,followers:demoProfile.followers,following:demoProfile.following,postIds:demoProfile.postIds,avatarUrl:avatarPath);
-  @override Future<String?> uploadAvatar(List<int> bytes,String extension,String? mimeType) async => null;
+  @override
+  Future<ProfileData> updateProfile({required String displayName, required String username, required String bio, String? avatarPath, String? gender}) async => ProfileData(id: demoProfile.id, displayName: displayName, handle: '@${username.replaceFirst(RegExp(r'^@+'), '')}', bio: bio, isCreator: demoProfile.isCreator, followers: demoProfile.followers, following: demoProfile.following, postIds: demoProfile.postIds, avatarUrl: avatarPath);
+  @override Future<String?> uploadAvatar(List<int> bytes, String extension, String? mimeType) async => null;
 }
