f4e527a3e6257481cecd2d8a5f46ceb3f02b7253
Manojpatil7711 <90497823+Manojpatil7711@users.noreply.github.com>
2026-09-24 15:30:31 +0530

    Route publishing through authenticated Edge Function

--- lib/features/home/data/supabase_post_repository.dart
+++ lib/features/home/data/supabase_post_repository.dart
@@ -118,7 +118,7 @@ class SupabasePostRepository {
       'allow_comments':allowComments,'allow_downloads':allowDownloads,
     }).select('id').single();
     final id=row['id'] as String;
-    await _client.rpc('publish_content_secure', params:{'p_content_id':id});
+    await _client.functions.invoke('manox_publish_content', body:{'content_id':id});
     final post=await _client.from('contents').select(_contentSelect).eq('id',id).single();
     final profile=post['profiles'] as Map<String,dynamic>?;
     final urls=post['media_urls'] as List?;
